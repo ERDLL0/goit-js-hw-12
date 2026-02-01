a0c57d1f666969e0293d938b48524f4bfc814859
Aslıhan <erdallasll@gmail.com>
2026-02-02 00:22:21 +0300

    main

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  define: {
-    global: 'window',
-  },
-});
-
-export default {
   base: '/goit-js-hw-12/', 
-};
+});
